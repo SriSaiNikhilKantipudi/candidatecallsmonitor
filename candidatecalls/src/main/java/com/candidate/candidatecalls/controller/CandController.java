16e31548b95c8b5fd1c708ca0701604e69a88821
ntihindukkipati <nithindukkipati@gmail.com>
2021-06-19 13:55:14 -0500

    CORS_Issue

--- candidatecalls/src/main/java/com/candidate/candidatecalls/controller/CandController.java
+++ candidatecalls/src/main/java/com/candidate/candidatecalls/controller/CandController.java
@@ -9,15 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.candidate.candidatecalls.model.Authentication;
 //import com.candidate.candidatecalls.model.Authentication;
@@ -29,6 +21,7 @@ import com.candidate.candidatecalls.service.CanService;
 
 @Component
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1")
 public class CandController {
 	
